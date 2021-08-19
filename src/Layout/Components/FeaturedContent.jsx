7dfcfbd910bccb1708ece9422a693b2e690c2277
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 16:58:06 -0700

    fixed width

--- src/Layout/Components/FeaturedContent.jsx
+++ src/Layout/Components/FeaturedContent.jsx
@@ -14,6 +14,7 @@ export default function FeaturedContent() {
       <Link
         to={`/blog/${card.category}/${card.id}`}
         className="text-img-container"
+        onClick={scrollToTop}
       >
         <img
           src={card.img}
@@ -22,44 +23,46 @@ export default function FeaturedContent() {
           style={{ width:"100%" }}
         />
         <div className="title-link featured-text">
-          <h1 className="mx-auto">{card.title}</h1>
+          <p>{card.title}</p>
         </div>
       </Link>
     </div>
   )
   
   return (
-    <section id="visual-nav" className="cards-section wf-section">
-      <h1 className="heading-6">Featured Content</h1>
-      <FeaturedBlog card={data[0]}/>
-      <div className="w-container" data-aos="fade-up">
-        <div className="flex-center">
-          <div className="intro-box">
-            <div className="intro-text-box">
-              <Link to="/blog/writing" className="title-link" onClick={scrollToTop}>Writing</Link>
-              <div className="top-border _5">
-                <Link to="/blog/writing" className="text-link" onClick={scrollToTop}>Blog posts and more...</Link>
+    <div className="w-container">
+      <section id="visual-nav" className="cards-section">
+        <h1 className="heading-6">Featured Content</h1>
+        <FeaturedBlog card={data[0]}/>
+        <div className="w-container" data-aos="fade-up">
+          <div className="flex-center">
+            <div className="intro-box">
+              <div className="intro-text-box">
+                <Link to="/blog/writing" className="title-link" onClick={scrollToTop}>Writing</Link>
+                <div className="top-border _5">
+                  <Link to="/blog/writing" className="text-link" onClick={scrollToTop}>Blog posts and more...</Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="intro-box-alt1">
-            <div className="intro-text-box">
-              <Link to="/blog/podcasts" className="title-link" onClick={scrollToTop}>Podcasts</Link>
-              <div className="top-border _5">
-                <Link to="/blog/podcasts" className="text-link" onClick={scrollToTop}>Features and guest appearances</Link>
+            <div className="intro-box-alt1">
+              <div className="intro-text-box">
+                <Link to="/blog/podcasts" className="title-link" onClick={scrollToTop}>Podcasts</Link>
+                <div className="top-border _5">
+                  <Link to="/blog/podcasts" className="text-link" onClick={scrollToTop}>Features and guest appearances</Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="intro-box-alt2 no-border">
-            <div className="intro-text-box">
-              <Link to="/blog/teaching" className="title-link" onClick={scrollToTop}>Teaching</Link>
-              <div className="top-border _5">
-                <Link to="/blog/teaching" className="text-link" onClick={scrollToTop}>Recorded lectures and teaching</Link>
+            <div className="intro-box-alt2 no-border">
+              <div className="intro-text-box">
+                <Link to="/blog/teaching" className="title-link" onClick={scrollToTop}>Teaching</Link>
+                <div className="top-border _5">
+                  <Link to="/blog/teaching" className="text-link" onClick={scrollToTop}>Recorded lectures and teaching</Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
