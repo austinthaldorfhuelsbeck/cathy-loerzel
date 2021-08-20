4ea430ffdc98e1f955a0e512c5b6426f1c58daa2
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 20:24:17 -0700

    add featured content

--- src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
+++ src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
@@ -1,11 +1,15 @@
 import ProductBanner from "../Components/ProductBanner"
 import Testimonials from "./Testimonials"
+import FeaturedContent from "../Components/FeaturedContent"
 
 export default function RedeemingHeartache() {
   return (
-    <section className="rh-section">
-      <ProductBanner />
-      <Testimonials />
-    </section>
+    <>
+      <section className="rh-section">
+        <ProductBanner />
+        <Testimonials />
+      </section>
+      <FeaturedContent />
+    </>
   )
 }
