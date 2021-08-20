e3ed894f6aec99ed69a79b2ed244e2e587da5a43
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 19:55:21 -0700

    testimonials carousel map

--- src/Layout/RedeemingHeartache/Testimonials.jsx
+++ src/Layout/RedeemingHeartache/Testimonials.jsx
@@ -1,5 +1,7 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
+
+import testimonials from "../../data/testimonials"
 
 export default function Testimonials() {
   return (
@@ -10,32 +12,15 @@ export default function Testimonials() {
         showThumbs={false}
         showStatus={false}
         autoPlay={true}
-        interval={6100}
+        interval={16100}
       >
-        <div className="myCarousel">
-          <h3>Shirley Fultz</h3>
-          <h4>Designer</h4>
-          <p>
-            It's freeing to be able to catch up on customized news and not be
-            distracted by a social media element on the same site
-          </p>
-        </div>
-        <div className="myCarousel">
-          <h3>Daniel Keystone</h3>
-          <h4>Designer</h4>
-          <p>
-            The simple and intuitive design makes it easy for me use. I highly
-            recommend Fetch to my peers.
-          </p>
-        </div>
-        <div className="myCarousel">
-          <h3>Theo Sorel</h3>
-          <h4>Designer</h4>
-          <p>
-            I enjoy catching up with Fetch on my laptop, or on my phone when
-            I'm on the go!
-          </p>
-        </div>
+        {testimonials.map((testimonial, index) => (
+          <div className="myCarousel">
+            <h3>{testimonial.name}</h3>
+            <h4>{testimonial.title}</h4>
+            <p>{testimonial.message}</p>
+          </div>
+        ))}
       </Carousel>
     </div>
   )
---
+++ src/data/testimonials.jsx
@@ -0,0 +1,19 @@
+const testimonials = [
+  {
+    name: "Rick Sanchez",
+    title: "sub1",
+    message: "Lorem ipsum!"
+  },
+  {
+    name: "Bird Person",
+    title: "sub2",
+    message: "Lorem ipsum!"
+  },
+  {
+    name: "Morty Smith",
+    title: "sub3",
+    message: "Lorem ipsum!"
+  }
+]
+
+export default testimonials
