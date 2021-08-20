7282d1ed31a77367bf17a97521b253a79ec3ec69
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 19:18:55 -0700

    carousel component init

--- src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
+++ src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
@@ -1,10 +1,12 @@
 import ProductBanner from "../Components/ProductBanner"
+import Testimonials from "./Testimonials"
 
 export default function RedeemingHeartache() {
   return (
     <div>
       <h1 className="h1-redeeming my-5">Redeeming <em>Heartache</em></h1>
       <ProductBanner />
+      <Testimonials />
     </div>
   )
 }
