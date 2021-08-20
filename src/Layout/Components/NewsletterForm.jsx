abfe6f6fab81265c80f552c1bf91904266e3d37f
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 17:38:07 -0700

    add form

--- src/Layout/Components/NewsletterForm.jsx
+++ src/Layout/Components/NewsletterForm.jsx
@@ -1,3 +1,5 @@
+import Mailchimp from "react-mailchimp-form"
+
 export default function NewsletterForm() {
   return (
     <div id="newsletter-form" className="content-section wf-section">
@@ -13,48 +15,24 @@ export default function NewsletterForm() {
           <div className="top-border-2 _10">
             <p data-aos="fade-up">Get promotions &amp; updates!</p>
             <div className="top-border _20">
-              <div data-w-id="aeeab1da-d2d2-7ef0-22de-ce43fdd06b3c" className="form-block-2 w-form">
-                <form
-                  data-name="Newsletter"
-                  name="wf-form-Newsletter"
-                  className="flex-2"
-                >
-                <input
-                  type="text"
-                  className="text-field-2 w-input"
-                  maxlength="256"
-                  name="Name"
-                  data-name="Name"
-                  placeholder="Your name"
-                  data-aos="fade-up"
-                  required
+              <div className="form-block-2 w-form">
+                <Mailchimp
+                  action="https://austinthaldorfhuelsbeck.us16.list-manage.com/subscribe/post?u=07d928836c4bfc5f2ac3958b0&amp;id=da7fe51007"
+                  fields={[
+                    {
+                      name: "EMAIL",
+                      placeholder: "email",
+                      type: "email",
+                      required: true,
+                    },
+                    {
+                      name: "NAME",
+                      placeholder: "your name",
+                      type: "text",
+                      required: false,
+                    }
+                  ]}
                 />
-                <input
-                  type="email"
-                  className="text-field-2 _2 w-input"
-                  maxlength="256"
-                  name="Email"
-                  data-name="Email"
-                  placeholder="Your email"
-                  data-aos="fade-up"
-                  required
-                />
-                <input
-                  type="submit"
-                  value="Subscribe"
-                  data-wait="Please wait..."
-                  className="submit-button-2 w-button"
-                  data-aos="fade-up"
-                />
-                </form>
-                <div className="success-message-2 w-form-done" data-aos="fade-up">
-                  <div className="top-border-2">
-                    <div>Thank you! Your submission has been received!</div>
-                  </div>
-                </div>
-                <div className="error-message w-form-fail" data-aos="fade-up">
-                  <div>Oops! Something went wrong while submitting the form.</div>
-                </div>
               </div>
             </div>
           </div>
