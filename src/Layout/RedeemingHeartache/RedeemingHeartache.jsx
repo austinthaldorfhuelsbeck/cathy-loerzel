9af8ba7f50dcb084d1c095765c04401ad370eede
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 19:12:27 -0700

    page title

--- src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
+++ src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
@@ -3,6 +3,7 @@ import ProductBanner from "../Components/ProductBanner"
 export default function RedeemingHeartache() {
   return (
     <div>
+      <h1 className="h1-redeeming my-5">Redeeming <em>Heartache</em></h1>
       <ProductBanner />
     </div>
   )
