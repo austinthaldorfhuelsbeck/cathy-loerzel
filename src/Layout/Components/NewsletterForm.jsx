5eb3944c0a6478f8e99acc547324cebca30e2ab0
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-19 17:54:24 -0700

    form style begin

--- src/Layout/Components/NewsletterForm.jsx
+++ src/Layout/Components/NewsletterForm.jsx
@@ -14,26 +14,24 @@ export default function NewsletterForm() {
         <div className="_49-column-2">
           <div className="top-border-2 _10">
             <p data-aos="fade-up">Get promotions &amp; updates!</p>
-            <div className="top-border _20">
-              <div className="form-block-2 w-form">
-                <Mailchimp
-                  action="https://gmail.us5.list-manage.com/subscribe/post?u=07d928836c4bfc5f2ac3958b0&amp;id=da7fe51007"
-                  fields={[
-                    {
-                      name: "EMAIL",
-                      placeholder: "email",
-                      type: "email",
-                      required: true,
-                    },
-                    {
-                      name: "NAME",
-                      placeholder: "your name",
-                      type: "text",
-                      required: false,
-                    }
-                  ]}
-                />
-              </div>
+            <div className="form-block-2 w-form">
+              <Mailchimp
+                action="https://gmail.us5.list-manage.com/subscribe/post?u=07d928836c4bfc5f2ac3958b0&amp;id=da7fe51007"
+                fields={[
+                  {
+                    name: "EMAIL",
+                    placeholder: "email",
+                    type: "email",
+                    required: true,
+                  },
+                  {
+                    name: "NAME",
+                    placeholder: "your name",
+                    type: "text",
+                    required: false,
+                  }
+                ]}
+              />
             </div>
           </div>
         </div>
