679de720330de4a65a0424b9f6ba6d56ce1fad17
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-25 19:57:11 -0700

    straighten out layout

--- client/src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
+++ client/src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
@@ -11,11 +11,24 @@ export default function RedeemingHeartache() {
   return (
     <>
       <section className="rh-section">
-        <ProductBanner />
-        <div className="w-container">
-          <ReactPlayer url={videoSrc} className="mx-auto pt-5" width="75%" />
+        <div className="w-container py-5">
+          //
         </div>
-        <Testimonials />
+        <ProductBanner />
+        <div className="w-container py-5">
+          <h3><strong>Find freedom and healing</strong> from painful memories and relational struggles and learn how your past has uniquely prepared you to experience more joy.</h3>
+          <p>Tragedy and pain inevitably touch our lives in some way. We long to feel whole, but more often than not, the way we’ve learned to deal with our wounds pushes us away from the very restoration we need most. Renowned psychologist Dr. Dan Allender and counselor and teacher Cathy Loerzel present a life-changing process of true connection and healing with ourselves, God, and others.</p>
+          <ReactPlayer url={videoSrc} className="mx-auto my-5" width="75%" />
+          <p>With a clear, biblically trustworthy method, Allender and Loerzel walk you through a journey of profound inner transformation–from the shame and hurt of old emotional wounds to true freedom and healing. Drawn from modern research and their pioneering work at The Allender Center, they will help you identify your core trauma in one of the three outcast archetypes–the widow, orphan, or stranger–and chart your path of growth into the God-given roles of priest, prophet, or leader. This book will help you learn:</p>
+          <ul>
+            <li>What to do about feeling out-of-place and directionless</li>
+            <li>How your coping mechanisms create a false sense of health</li>
+            <li>How to embrace your divine calling and find lasting reconciliation</li>
+            <li>How your heart wounds are your unique invitation to true strength and purpose.</li>
+          </ul>
+          <Testimonials />
+          <h3>Your past pain does not dictate your life. Answer the call to healing and discover your life’s beautiful story and a future of hope and freedom.</h3>
+        </div>
       </section>
       <FeaturedBlog />
       <FeaturedContent />
