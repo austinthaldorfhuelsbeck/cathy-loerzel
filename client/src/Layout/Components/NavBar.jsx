66e59de1a16ac56f2bb324214bcf750c5284e077
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-25 18:15:22 -0700

    style nicer

--- client/src/Layout/Components/NavBar.jsx
+++ client/src/Layout/Components/NavBar.jsx
@@ -1,12 +1,12 @@
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap"
 
 export default function NavBar() {
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    })
-  }
+  // function scrollToTop() {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: "smooth",
+  //   })
+  // }
 
   return (
     <Navbar bg="light" expand="lg">
@@ -21,16 +21,16 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/#about">About</Nav.Link>
-            <NavDropdown title="Offerings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/writing">Writing</NavDropdown.Item>
-              <NavDropdown.Item href="/podcasts">Podcasts</NavDropdown.Item>
-              <NavDropdown.Item href="/teaching">Teaching</NavDropdown.Item>
+            <Nav.Link href="/#about" className="mx-2">About</Nav.Link>
+            <NavDropdown title="Offerings" id="basic-nav-dropdown" className="mx-2">
+              <NavDropdown.Item href="/blog/writing">Writing</NavDropdown.Item>
+              <NavDropdown.Item href="/blog/podcasts">Podcasts</NavDropdown.Item>
+              <NavDropdown.Item href="/blog/teaching">Teaching</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/coaching">Coaching</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/redeeming-heartache">Redeeming Heartache</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/redeeming-heartache" className="mx-2">Redeeming Heartache</Nav.Link>
+            <Nav.Link href="/contact" className="mx-2">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
