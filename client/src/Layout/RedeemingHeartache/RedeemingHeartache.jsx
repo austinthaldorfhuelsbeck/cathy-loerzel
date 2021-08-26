3a9e9effe66fef17db0521cb6ac09ee1ae6c28d2
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-08-25 19:37:04 -0700

    add vid

--- client/src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
+++ client/src/Layout/RedeemingHeartache/RedeemingHeartache.jsx
@@ -1,13 +1,20 @@
+import ReactPlayer from "react-player"
+
 import ProductBanner from "../Components/ProductBanner"
 import Testimonials from "./Testimonials"
 import FeaturedBlog from "../Components/FeaturedContent/FeaturedBlog"
 import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
 
 export default function RedeemingHeartache() {
+  const videoSrc = "https://www.youtube.com/watch?v=sToxP2Q3hLM"
+
   return (
     <>
       <section className="rh-section">
         <ProductBanner />
+        <div className="w-container">
+          <ReactPlayer url={videoSrc} className="mx-auto pt-5" width="75%" />
+        </div>
         <Testimonials />
       </section>
       <FeaturedBlog />
