6e2799cd9de189b059f4132c10ab1964394c7bbe
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-09-01 20:29:05 -0700

    Active on Events page by default

--- client/src/Layout/Home/Home.jsx
+++ client/src/Layout/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Hero from "./Hero"
 import NewsletterForm from "../Components/NewsletterForm"
+import ProductBanner from "../Components/ProductBanner"
 import About from "./About"
 import UpcomingEvents from "../Components/UpcomingEvents/UpcomingEvents"
-import ProductBanner from "../Components/ProductBanner"
-import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
-import FeaturedBlog from "../Components/FeaturedContent/FeaturedBlog"
 import ContactForm from "../Components/ContactForm"
+import FeaturedBlog from "../Components/FeaturedContent/FeaturedBlog"
+import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
+import InstagramFeed from "../Components/InstagramFeed"
 
 export default function Home() {  
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <ContactForm />
       <FeaturedBlog />
       <FeaturedContent />
+      <InstagramFeed />
     </div>
   )
 }
