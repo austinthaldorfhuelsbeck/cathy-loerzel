fcc62cbc27a0d84964c86357252ad9abad915afc
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-09-02 12:30:08 -0700

    testimonials come from api

--- client/src/Layout/Blog/Blog.jsx
+++ client/src/Layout/Blog/Blog.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import {  Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
 import Card from "./Card"
