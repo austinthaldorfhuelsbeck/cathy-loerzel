f6fccbcdb4222a9496e420d0c4f9f26452d58009
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-04-01 15:10:52 -0700

    icons to footer

--- client/src/Layout/Home/Home.jsx
+++ client/src/Layout/Home/Home.jsx
@@ -1,25 +1,23 @@
 import Hero from "./Hero"
-import NewsletterForm from "../Components/NewsletterForm"
-import ProductBanner from "../Components/ProductBanner"
-import About from "./About"
-import UpcomingEvents from "../Components/UpcomingEvents/UpcomingEvents"
-import ContactForm from "../Components/ContactForm"
-import FeaturedBlog from "../Components/FeaturedContent/FeaturedBlog"
-import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
-import InstagramFeed from "../Components/InstagramFeed"
+// import NewsletterForm from "../Components/NewsletterForm"
+// import ProductBanner from "../Components/ProductBanner"
+// import About from "./About"
+// import UpcomingEvents from "../Components/UpcomingEvents/UpcomingEvents"
+// import ContactForm from "../Components/ContactForm"
+// import FeaturedBlog from "../Components/FeaturedContent/FeaturedBlog"
+// import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
 
 export default function Home() {  
   return (
     <div>
       <Hero />
-      <NewsletterForm />
+      {/* <NewsletterForm />
       <ProductBanner />
       <About />
       <UpcomingEvents />
       <ContactForm />
       <FeaturedBlog />
-      <FeaturedContent />
-      <InstagramFeed />
+      <FeaturedContent /> */}
     </div>
   )
 }
