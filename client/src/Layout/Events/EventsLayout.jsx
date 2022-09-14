274378c4904d842f96668cf7167b8f34ba86d7c3
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-09-13 22:21:04 -0700

    event types sorting

--- client/src/Layout/Events/EventsLayout.jsx
+++ client/src/Layout/Events/EventsLayout.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom"
 import Events from "./Events"
 import NewsletterForm from "../Components/NewsletterForm"
 import ProductBanner from "../Components/ProductBanner"
-import FeaturedContent from "../Components/FeaturedContent/FeaturedContent"
+import EventTypes from "../Components/EventTypes"
 
 export default function EventsLayout() {
   let { type } = useParams()
@@ -13,7 +13,7 @@ export default function EventsLayout() {
       <div className="blog-banner">
         <div className="blog-text-box">
           <h1 className="blog-header" data-aos="fade" data-aos-duration="100000">
-            {type ? type : ""}
+            {type ? type : "Story Work"}
           </h1>
         </div>
       </div>
@@ -26,14 +26,36 @@ export default function EventsLayout() {
     </>
   )
 
+  const EventsHeader = ({ type }) => {
+    if (type === "coaching") return (
+      <div className="w-container">
+        <h2>STORY WORK AND LEADERSHIP COACHING</h2>
+        <p>Leading an organization, being a therapist, or running groups is difficult work that needs care and attunement to your own story as you tend to others. Cathy offers 9 coaching sessions to help you do some story work, understand where you are in your formational trajectory and gain clarity and wisdom as to your next steps in your journey toward wholeness.</p>
+        <p><em>Coaching sessions: $200 with a sliding scale if needed.</em></p>
+      </div>
+    )
+    return <></>
+  }
+
+  const EventsList = ({ type }) => {
+    if (type === "coaching" || type === "intensives") {
+      return <></>
+    }
+    if (type === "retreats" || type === "conferences") {
+      return <Events type={type} />
+    }
+    return <Events />
+  }
+
   return (
     <div>
       <EventsBanner />
+      <EventsHeader type={type} />
       <div className="w-container upcoming-section">
-        {type ? <Events type={type} /> : <Events />}
+        <EventsList type={type} />
       </div>
+      <EventTypes />
       <NewsletterForm />
-      <FeaturedContent />
       <ProductBanner />
     </div>
   )
