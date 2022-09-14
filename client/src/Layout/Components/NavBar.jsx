88e64441d838c260074cbff305eb20f0a5ae0e79
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-09-13 20:58:49 -0700

    Story Work in navbar

--- client/src/Layout/Components/NavBar.jsx
+++ client/src/Layout/Components/NavBar.jsx
@@ -15,7 +15,13 @@ export default function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/events" className="mx-2">Events</Nav.Link>
-            <Nav.Link href="/redeeming-heartache" className="mx-2">Redeeming Heartache</Nav.Link>
+            {/* <Nav.Link href="/redeeming-heartache" className="mx-2">Redeeming Heartache</Nav.Link> */}
+            <NavDropdown title="Story Work" id="basic-nav-dropdown" className="mx-2">
+              <NavDropdown.Item href="">Coaching</NavDropdown.Item>
+              <NavDropdown.Item href="">Intensives</NavDropdown.Item>
+              <NavDropdown.Item href="">Retreats</NavDropdown.Item>
+              <NavDropdown.Item href="">Conferences</NavDropdown.Item>
+            </NavDropdown>
             <NavDropdown title="Resources" id="basic-nav-dropdown" className="mx-2">
               <NavDropdown.Item href="/blog/writing">Writing</NavDropdown.Item>
               <NavDropdown.Item href="/blog/podcasts">Podcasts</NavDropdown.Item>
