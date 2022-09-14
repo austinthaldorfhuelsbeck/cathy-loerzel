fd4a36ee983403e74ccb039cd93f0f205bf972c9
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-09-13 20:43:29 -0700

    add events type; add isUpcoming; display correctly

--- client/src/Layout/Components/UpcomingEvents/UpcomingEvents.jsx
+++ client/src/Layout/Components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,27 +1,34 @@
 import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
 import AccordionItem from "./AccordionItem"
 
 export default function UpcomingEvents() {
   const [events, setEvents] = useState([])
   const [err, setErr] = useState("")
+  const location = useLocation()
 
   useEffect(() => {
     let url = `${process.env.REACT_APP_API_BASE_URL}/events`
-    console.log("URL: ", url)
+    // console.log("URL: ", url)
     fetch(url)
       .then(res => res.json())
       .then((res) => setEvents(res.data))
       .catch(setErr)
   }, [])
 
+  console.log(location.pathname)
+
   return (
     <div className="w-container upcoming-section">
       {err}
-      {console.log("EVENTS: " + events[0])}
+      {/* {console.log("EVENTS: " + events[0])} */}
       <h3 className="lg-title">Upcoming Events</h3>
       <div className="my-5">
-        {events.slice(0,3).map((event) => (
-          <AccordionItem key={event.title} item={event} />
+        {events
+          .filter((event) => event.isUpcoming)
+          .slice(location.pathname === "/events" ? "()" : "(0, 3)")
+          .map((event) => (
+            event.isUpcoming && <AccordionItem key={event.title} item={event} />
         ))}
       </div>
     </div>
