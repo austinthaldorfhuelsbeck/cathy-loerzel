322be6ddf17f86d51ec93ec59cbb8ace3d9f0350
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-09-13 22:28:52 -0700

    conditional layout

--- client/src/Layout/Events/EventsLayout.jsx
+++ client/src/Layout/Events/EventsLayout.jsx
@@ -4,6 +4,7 @@ import Events from "./Events"
 import NewsletterForm from "../Components/NewsletterForm"
 import ProductBanner from "../Components/ProductBanner"
 import EventTypes from "../Components/EventTypes"
+import ContactForm from "../Components/ContactForm"
 
 export default function EventsLayout() {
   let { type } = useParams()
@@ -28,10 +29,38 @@ export default function EventsLayout() {
 
   const EventsHeader = ({ type }) => {
     if (type === "coaching") return (
+      <>
+        <div className="w-container">
+          <h2>STORY WORK AND LEADERSHIP COACHING</h2>
+          <p>Leading an organization, being a therapist, or running groups is difficult work that needs care and attunement to your own story as you tend to others. Cathy offers 9 coaching sessions to help you do some story work, understand where you are in your formational trajectory and gain clarity and wisdom as to your next steps in your journey toward wholeness.</p>
+          <p><em>Coaching sessions: $200 with a sliding scale if needed.</em></p>
+        </div>
+        <ContactForm type={type} />
+      </>
+    )
+    if (type === "intensives") return (
+      <>
+        <div className="w-container">
+          <h2>INTENSIVES</h2>
+          <p>Intensives are an opportunity for you to go deeper into your own story and continue your journey into understanding how your past pain impacts the way you currently interact with yourself and with the world.</p>
+          <p>During an intensive, Cathy will work with two of your stories and help unpack themes and underlying parts of your story that will help move you toward more freedom and joy.</p>
+          <p>An intensive consists of 2 days from 8am - 2pm.  These sessions take place in her office in Seattle, WA. She can meet with an individual, group, or couple and is currently booking fall and spring sessions.</p>
+          <p><em>Intensives: $4,000 with a sliding scale if needed.</em></p>
+        </div>
+        <ContactForm type={type} />
+      </>
+    )
+    if (type === "retreats") return (
       <div className="w-container">
-        <h2>STORY WORK AND LEADERSHIP COACHING</h2>
-        <p>Leading an organization, being a therapist, or running groups is difficult work that needs care and attunement to your own story as you tend to others. Cathy offers 9 coaching sessions to help you do some story work, understand where you are in your formational trajectory and gain clarity and wisdom as to your next steps in your journey toward wholeness.</p>
-        <p><em>Coaching sessions: $200 with a sliding scale if needed.</em></p>
+        <h2>RETREATS</h2>
+        <p>Cathy is in the process of designing retreats that combine story and trauma work as well as body work and prayer/spiritual healing. If you are interested in bringing her in to run a retreat for your community, she would love to collaborate with you.</p>
+      </div>
+    )
+    if (type === "conferences") return (
+      <div className="w-container">
+        <h2>CONFERENCES</h2>
+        <p>Cathy is available to speak to your community, church, or organization. She teaches on the Orphan, Widow, Stranger content found in Redeeming Heartache in depth.</p>
+        <p>She also teaches on trauma, story and healing, spiritual warfare, and her hallmark model called the “U-Diagram” on the trajectory of healing the human heart.</p>
       </div>
     )
     return <></>
