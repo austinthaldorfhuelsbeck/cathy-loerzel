f103393f829222498a3d4f951aaaea6d5ac8547f
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2023-08-20 00:37:24 -0700

    react-mailchimp-form => use-mailchimp-form

--- client/src/Layout/Components/NewsletterForm.jsx
+++ client/src/Layout/Components/NewsletterForm.jsx
@@ -1,6 +1,19 @@
-import Mailchimp from "react-mailchimp-form"
+import { useFormFields, useMailChimpForm } from "use-mailchimp-form"
 
 export default function NewsletterForm() {
+  const url = "https://gmail.us5.list-manage.com/subscribe/post?u=07d928836c4bfc5f2ac3958b0&amp;id=da7fe51007"
+  const {
+    loading,
+    error,
+    success,
+    message,
+    handleSubmit
+  } = useMailChimpForm(url)
+  const { fields, handleFieldChange } = useFormFields({
+    EMAIL: "",
+    NAME: ""
+  })
+
   return (
     <div id="newsletter-form" className="content-section wf-section">
       <div className="content-wrapper w-container">
@@ -15,23 +28,37 @@ export default function NewsletterForm() {
           <div className="top-border-2 _10">
             <p data-aos="fade-up">Get promotions &amp; updates!</p>
             <div className="form-block-2">
-              <Mailchimp
-                action="https://gmail.us5.list-manage.com/subscribe/post?u=07d928836c4bfc5f2ac3958b0&amp;id=da7fe51007"
-                fields={[
-                  {
-                    name: "EMAIL",
-                    placeholder: "email",
-                    type: "email",
-                    required: true,
-                  },
-                  {
-                    name: "NAME",
-                    placeholder: "your name",
-                    type: "text",
-                    required: false,
-                  }
-                ]}
+            <form
+              onSubmit={event => {
+                event.preventDefault();
+                handleSubmit(fields);
+              }}
+            >
+              <input
+                id="EMAIL"
+                placeholder="email"
+                size="150"
+                autoFocus
+                type="email"
+                value={fields.EMAIL}
+                className="input"
+                onChange={handleFieldChange}
               />
+              <input
+                id="NAME"
+                placeholder="your name"
+                size="150"
+                autoFocus
+                type="email"
+                value={fields.NAME}
+                className="input"
+                onChange={handleFieldChange}
+              />
+              <button>Subscribe!</button>
+            </form>
+            {loading && "submitting"}
+            {error && message}
+            {success && message}
             </div>
           </div>
         </div>
