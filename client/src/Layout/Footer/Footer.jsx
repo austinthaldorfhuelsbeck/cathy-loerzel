c64433cce2df888a2c98bcde2f2da441ffe64cdb
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2023-08-21 20:35:47 -0700

    coaching page content update

--- client/src/Layout/Footer/Footer.jsx
+++ client/src/Layout/Footer/Footer.jsx
@@ -16,24 +16,24 @@ export default function Footer() {
         <div className="flex-space">
           <div className="_90-column">
             <div className="footer-links">
-              <Link to="/" className="footer-link" onClick={scrollToTop}>Home</Link>
-              <br />
               <Link to="/about" className="footer-link" onClick={scrollToTop}>About</Link>
               <br />
-              <Link to="/blog" className="footer-link" onClick={scrollToTop}>Blog</Link>
-              <Link to="/blog/writing" className="footer-link-alt" onClick={scrollToTop}>&#10551;Writing</Link>
-              <Link to="/blog/podcasts" className="footer-link-alt" onClick={scrollToTop}>&#10551;Podcasts</Link>
-              <Link to="/blog/teaching" className="footer-link-alt" onClick={scrollToTop}>&#10551;Teaching</Link>
+              <Link to="/contact" className="footer-link" onClick={scrollToTop}>Contact</Link>
               <br />
               <Link to="/redeeming-heartache" className="footer-link" onClick={scrollToTop}>Redeeming Heartache</Link>
-              <br />
+            </div>
+            <div className="footer-links">
               <Link to="/events" className="footer-link" onClick={scrollToTop}>Events</Link>
               <Link to="/events/retreats" className="footer-link-alt" onClick={scrollToTop}>&#10551;Retreats</Link>
               <Link to="/events/conferences" className="footer-link-alt" onClick={scrollToTop}>&#10551;Conferences</Link>
               <Link to="/events/coaching" className="footer-link-alt" onClick={scrollToTop}>&#10551;Coaching</Link>
               <Link to="/events/intensives" className="footer-link-alt" onClick={scrollToTop}>&#10551;Intensives</Link>
               <br />
-              <Link to="/contact" className="footer-link" onClick={scrollToTop}>Contact</Link>
+              <Link to="/blog" className="footer-link" onClick={scrollToTop}>Blog</Link>
+              <Link to="/blog/writing" className="footer-link-alt" onClick={scrollToTop}>&#10551;Writing</Link>
+              <Link to="/blog/podcasts" className="footer-link-alt" onClick={scrollToTop}>&#10551;Podcasts</Link>
+              <Link to="/blog/teaching" className="footer-link-alt" onClick={scrollToTop}>&#10551;Teaching</Link>
+              <br />
             </div>
           </div>
         </div>
